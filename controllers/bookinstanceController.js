56f91657d09591118d773689bf01f7cb8b3d8a54
Kishor Dhakal <compboykishor@gmail.com>
2021-05-25 14:33:39 -0700

    added new

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -91,20 +91,94 @@ exports.bookinstance_create_post = [
 ];
 // Display BookInstance delete form on GET.
 exports.bookinstance_delete_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance delete GET');
+    async.parallel({
+        bookinstance: function(callback) {
+            BookInstance.findById(req.params.id).exec(callback)
+        },
+    }, function(err, results) {
+        if (err) { return next(err); }
+        if (results.bookinstance==null) { // No results.
+            res.redirect('/catalog/bookinstances');
+        }
+        // Successful, so render.
+        res.render('bookinstance_delete', { title: 'Delete Instance', bookinstance: results.bookinstance} );
+    });
 };
 
 // Handle BookInstance delete on POST.
 exports.bookinstance_delete_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance delete POST');
+    async.parallel({
+        bookinstance: function(callback) {
+          BookInstance.findById(req.body.bookinstanceid).exec(callback)
+        },
+    }, function(err, results) {
+        if (err) { return next(err); }
+       
+        // Author has no books. Delete object and redirect to the list of authors.
+        BookInstance.findByIdAndRemove(req.body.bookinstanceid, function deleteInstance(err) {
+            if (err) { return next(err); }
+            // Success - go to author list
+            res.redirect('/catalog/bookinstances')
+        })
+        
+    });
 };
 
-// Display BookInstance update form on GET.
 exports.bookinstance_update_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance update GET');
-};
+    // Get book, authors and genres for form.
+    async.parallel({
+        bookinstance: function(callback) {
+            BookInstance.findById(req.params.id).populate('book').exec(callback);
+        },
+        }, function(err, results) {
+            if (err) { return next(err); }
+            if (results.bookinstance==null) { // No results.
+                var err = new Error('Instance not found');
+                err.status = 404;
+                return next(err);
+            }
+            // Success.
+            res.render('bookinstance_form', { title: 'Update Instance', bookinstance: results.bookinstance });
+        });};
 
 // Handle bookinstance update on POST.
-exports.bookinstance_update_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance update POST');
-};
+// Handle book update on POST.
+exports.bookinstance_update_post = [
+
+    // Validate and sanitise fields.
+    body('book', 'Book must not be empty.').trim().isLength({ min: 1 }).escape(),
+    body('imprint', 'Imprint must not be empty.').trim().isLength({ min: 1 }).escape(),
+    body('status', 'Status must not be empty.').trim().isLength({ min: 1 }).escape(),
+    body('due_back', 'Due_back must not be empty').trim().isLength({ min: 1 }).escape(),
+
+    // Process request after validation and sanitization.
+    (req, res, next) => {
+
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        // Create a Book object with escaped/trimmed data and old id.
+        var bookinstance = new BookInstance(
+          { book: req.body.book,
+            imprint: req.body.imprint,
+            status: req.body.status,
+            due_back: req.body.due_back,
+           });
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render form again with sanitized values/error messages.
+
+            res.render('book_form', { title: 'Update Book',authors: results.authors, genres: results.genres, book: book, errors: errors.array() });
+            
+            return;
+        }
+        else {
+            // Data from form is valid. Update the record.
+            BookInstance.findByIdAndUpdate(req.params.id, bookinstance, {}, function (err,thebookinstance) {
+                if (err) { return next(err); }
+                   // Successful - redirect to book detail page.
+                   res.redirect(thebookinstance.url);
+                });
+        }
+    }
+];
